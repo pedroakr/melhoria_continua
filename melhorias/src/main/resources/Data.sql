345d03d74ad47f20b899310ad3d6e85bc26c2b77
pedroakr <pedro_kruger@outlook.com>
2025-09-18 17:01:14 -0300

    New

--- melhorias/src/main/resources/Data.sql
+++
@@ -1,4 +0,0 @@
--- Este script insere um usuário padrão para testes
--- A senha é 'senha123', criptografada com BCrypt
-INSERT INTO tb_usuarios (nome, usuario, email, senha_hash, perfil)
-VALUES ('Usuário de Teste', 'tester', 'teste@cottonstar.com', '$2a$10$3g5vJ5.ASn0g.1g1aX867.y3.Ld353wSME92APpP2M/A1D5b/K4.q', 'COLABORADOR');
